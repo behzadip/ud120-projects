678dbac7b40b21b70d81919e99b76fd1c6812d96
behzadip <behzad.iranpanah@gmail.com>
2016-08-21 10:53:51 -0400

    Added SVM model

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,6 +12,9 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+import numpy as np
 
 
 ### features_train and features_test are the features for the training
@@ -23,8 +26,27 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-### your code goes here ###
+mdl = SVC(kernel='linear', C=10000)
 
+features_train = features_train[:len(features_train)/100] 
+labels_train = labels_train[:len(labels_train)/100]
+
+t0 = time()
+mdl.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+t0 = time()
+pred = mdl.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
+print "accuracy", accuracy_score(pred, labels_test)
+
+print pred[10]
+print pred[26]
+print pred[50]
+
+print  np.count_nonzero(pred)
 #########################################################
 
+#training time: 127.915 s
+#prediction time: 13.752 s
+#accuracy 0.984072810011
 
