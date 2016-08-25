5d219d53d02b292d1e42bf262f356acd9fe0e62b
Behzad Iranpanah <Behzad@caminomodular.local>
2016-08-25 14:29:56 -0400

    Added decision tree model

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,6 +12,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -23,9 +25,20 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-### your code goes here ###
+
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+t0 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
+print "accuracy", accuracy_score(pred, labels_test)
 
 
 #########################################################
 
-
+#training time: 68.655 s
+#prediction time: 0.021 s
+#accuracy 0.977815699659
